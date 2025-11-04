42da8a4c6455406ba584f3671168b3bb2605992a
Robert Stolz <robert.stolz87@gmail.com>
2025-11-04 12:48:09 +0100

    4.2.1 angefangen

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ preis_kinder = 2.5
 preis_premium = 3.0
 preis_basis = 4.0
 preis_jugendlich = 3.5
+glas_sekt_preis = 0.75
+erwachsene_gesamt = 0
 
 
 def tarif_abfragen():
+    global erwachsene_gesamt
     print(" Hallo, geben Sie bitte Ihr Alter ein.")
     alter_gast = int(input())
 
@@ -23,6 +26,7 @@ def tarif_abfragen():
         print(" Wenn Sie Basis-Mitglied sind, geben Sie 'b' ein.")
         print(" Wenn Sie kein Mitglied sind, drücken Sie eine beliebige andere Taste. ")
         antwort_rabatt = input()
+        erwachsene_gesamt = erwachsene_gesamt + 1
 
         if antwort_rabatt == "p":
             print(" ### Eintritt Premium-Mitglied ### ")
@@ -46,5 +50,18 @@ while weitere_karte == "j":
     tarif_abfragen()
     print(" Möchten Sie noch eine weitere Karte berechnen? (j/n) ")
     weitere_karte = input()
+
+print(" Möchten Sie ein Glas Sekt für 0,75 Euro dazu? (j/n) ")
+antwort_sekt = input()
+
+if antwort_sekt == "j":
+    print(" Wie viele von den", erwachsene_gesamt , " Erwachsenen möchten Sekt? ")
+    anzahl_sekt = int(input())
+    sekt_preis_gesamt = anzahl_sekt * glas_sekt_preis
+
+    gesamt_preis = preis_erwachsene + sekt_preis_gesamt
+    print(" Gesamtpreis: ", gesamt_preis, " Euro ")
+else:
+    print(" Kein Sekt gewählt. ")
 
 print("Viel Spaß!")
