4452983d1716113aca305709b6e2597a3f41fef5
Robert Stolz <robert.stolz87@gmail.com>
2025-11-06 07:27:11 +0100

    kein sekt

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ preis_basis = 4.0
 preis_jugendlich = 3.5
 glas_sekt_preis = 0.75
 erwachsene_gesamt = 0
+gesamt_preis = 0.0
 
 
 def tarif_abfragen():
     global erwachsene_gesamt
+    global gesamt_preis
     print(" Hallo, geben Sie bitte Ihr Alter ein.")
     alter_gast = int(input())
 
     if alter_gast < 14:
         print(" ### Eintritt Kinder ### ")
         print(" Preis: ", preis_kinder, " Euro ")
+        gesamt_preis = gesamt_preis + preis_kinder
 
     elif 14 <= alter_gast <= 17:
         print(" ### Eintritt Jugendliche ### ")
         print(" Preis: ", preis_jugendlich, " Euro ")
+        gesamt_preis = gesamt_preis + preis_jugendlich
 
     else:
         print(" Sind Sie Mitglied im Duisburger Museumsclub? (Nachweis erforderlich) ")
@@ -31,19 +35,21 @@ def tarif_abfragen():
         if antwort_rabatt == "p":
             print(" ### Eintritt Premium-Mitglied ### ")
             print(" Preis: ", preis_premium, " Euro ")
+            gesamt_preis = gesamt_preis + preis_premium
 
         elif antwort_rabatt == "b":
             print(" ### Eintritt Basis-Mitglied ### ")
             print(" Preis: ", preis_basis, " Euro ")
+            gesamt_preis = gesamt_preis + preis_basis
         else:
             print(" ### Eintritt Erwachsene (voller Preis) ### ")
             print(" Preis: ", preis_erwachsene, " Euro")
+            gesamt_preis = gesamt_preis + preis_erwachsene
 
 
 print(" ### Tarifauskunftsrechner Museum XXX ### ")
 tarif_abfragen()
 
-weitere_karte = ""
 print(" Möchten Sie noch eine weitere Karte berechnen? (j/n) ")
 weitere_karte = input()
 while weitere_karte == "j":
@@ -54,17 +60,14 @@ while weitere_karte == "j":
 if erwachsene_gesamt > 0:
     print(" Möchten die Erwachsenen ein Glas Sekt je 0,75 Euro dazu? (j/n) ")
     antwort_sekt = input()
-else:
-    antwort_sekt = "n"
 
-if antwort_sekt == "j":
-    print(" Wie viele von den", erwachsene_gesamt , " Erwachsenen möchten Sekt? ")
-    anzahl_sekt = int(input())
-    sekt_preis_gesamt = anzahl_sekt * glas_sekt_preis
+    if antwort_sekt == "j":
+        print(" Wie viele von den", erwachsene_gesamt , " Erwachsenen möchten Sekt? ")
+        anzahl_sekt = int(input())
+        sekt_preis_gesamt = anzahl_sekt * glas_sekt_preis
+        gesamt_preis = gesamt_preis + sekt_preis_gesamt
+    else:
+        print(" Kein Sekt gewählt. ")
 
-    gesamt_preis = preis_erwachsene + sekt_preis_gesamt
-    print(" Gesamtpreis: ", gesamt_preis, " Euro ")
-else:
-    print(" Kein Sekt gewählt. ")
-
+print("Der Gesamtpreis beträgt: ", gesamt_preis, " Euro ")
 print("Viel Spaß!")
